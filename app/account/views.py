4994471f21dcfe35d95bd383238f96de5b8b9e0b
yousef <yousef@yousef.com>
2022-12-27 16:50:41 +0330

    using mixin for the first time!

--- app/account/views.py
+++ app/account/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
-
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import ListView
+from blog.models import Articles
 # Create your views here.
+
+@login_required
+def home(request):
+    template_name = "registration/home.html"
+    return render(request, template_name=template_name)
+
+
+class ArticleListView(ListView):
+    queryset = Articles.objects.published_manager()
+    template_name = "registration/home.html"
+
--- app/config/settings.py
+++ app/config/settings.py
@@ -57,11 +57,12 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'config.urls'
-
+LOGIN_REDIRECT_URL = "account:home"
+LOGIN_URL="account:login"
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,"templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
